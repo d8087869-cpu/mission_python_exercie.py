ff3456f415dd5b11d99bc32314beddca583405b9
david <d8087869@gmail.com>
2026-06-18 15:51:11 +0300

    added-mission-info

--- mission_control.py
+++ mission_control.py
@@ -11,9 +11,18 @@ print(type(distance_to_target))
 print(type(mission_active_status))
 #number 5 
 base_to_target_and_back = distance_to_target*2 
-print(base_to_target_and_back)
+print(f"base to target and back {base_to_target_and_back}")
 # number 6 
 fuel_usage = 3
 fuel_usage = int(fuel_usage)
-print(base_to_target_and_back*fuel_usage)
+fuel_usaeg_for_mission = base_to_target_and_back*fuel_usage
+print(f"fuel usaeg for mission{fuel_usaeg_for_mission}")
+#number 7 
+total_fuel = 50
+total_fuel = int(total_fuel)
+total_fule_after_mission = total_fuel - fuel_usaeg_for_mission
+print(f"total feul after mission {total_fule_after_mission}")
+#number 8 
+
+
 

711dd7904cb4e7713ed10434962c1a60e94d19e3
david <d8087869@gmail.com>
2026-06-18 14:42:19 +0300

    added_info_to_mission

---
+++ mission_control.py
@@ -0,0 +1,6 @@
+# number 2 
+agent_name = "david" 
+mission_code = 147
+distance_to_target = 5.5
+mission_active_status = False 
+print(f"agent name{agent_name},mission code{mission_code},distance to target{distance_to_target} , mission active status{mission_active_status}")
